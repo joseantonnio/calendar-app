74b25476bb19f0a4379c91d639ff9e00cfb809f0
José Antonio <jose.antonio@performmaonline.com>
2020-11-11 19:30:14 -0300

    Better views arrangement

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,33 +1,26 @@
-@extends('app')
+@extends('layouts/auth')
 
-@section('title', 'Login')
+@section('page', 'Register')
 
+@push('scripts')
+    <script src="{{ asset('js/register.js') }}"></script>
+@endpush
 
-    @push('styles')
-        <link type="text/css" rel="stylesheet" href="{{ asset('css/login.css') }}" />
-    @endpush
+@section('form', 'formRegister')
+@section('message', 'Sign Up')
+@section('button', 'Register')
 
+@section('fields')
+    <label for="inputName" class="sr-only">Name</label>
+    <input type="text" id="inputName" class="form-control top" placeholder="Name" required autofocus>
+    <label for="inputEmail" class="sr-only">Email address</label>
+    <input type="email" id="inputEmail" class="form-control middle" placeholder="Email address" required>
+    <label for="inputPassword" class="sr-only">Password</label>
+    <input type="password" id="inputPassword" class="form-control middle" placeholder="Password" required>
+    <label for="inputConfirm" class="sr-only">Confirm Password</label>
+    <input type="password" id="inputConfirm" class="form-control bottom" placeholder="Confirm Password" required>
+@endsection
 
-    @push('scripts')
-        <script src="{{ asset('js/register.js') }}"></script>
-    @endpush
-
-
-@section('content')
-    <form class="form-signin text-center" method="POST" id="formRegister">
-        <h1 class="h1 mb-3"><i class="mdi mdi-calendar"></i> Calendar App</h1>
-        <h2 class="h4 mb-3 font-weight-normal">Sign Up</h2>
-        <label for="inputName" class="sr-only">Name</label>
-        <input type="text" id="inputName" class="form-control top" placeholder="Name" required autofocus>
-        <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" id="inputEmail" class="form-control middle" placeholder="Email address" required>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control middle" placeholder="Password" required>
-        <label for="inputConfirm" class="sr-only">Confirm Password</label>
-        <input type="password" id="inputConfirm" class="form-control bottom" placeholder="Confirm Password" required>
-        <div class="checkbox mb-3">
-            <a href="{{ url('login') }}">Already a member? Log In</a>
-        </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
-    </form>
+@section('text')
+    <a href="{{ url('login') }}">Already a member? Log In</a>
 @endsection

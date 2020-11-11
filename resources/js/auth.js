dc530426986f7384d24426ebd3b239674c2c41ed
José Antonio <jose.antonio@performmaonline.com>
2020-11-11 19:46:29 -0300

    Better authentication system

--- resources/js/auth.js
+++
@@ -1,8 +0,0 @@
-var protected_routes = [
-    '/'
-];
-
-if (cookies.get('calendar_app_access_token') == undefined && protected_routes.includes(window.location.pathname)) {
-    document.body.innerHTML = "";
-    window.location.assign("/login");
-}

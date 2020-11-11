b5d6d391b21526b7ad5eb1efaf2704c7cb38f277
José Antonio <jose.antonio@performmaonline.com>
2020-11-11 19:30:45 -0300

    Fix AuthController login response

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -76,7 +76,7 @@ class AuthController extends Controller
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth('api')->factory()->getTTL() * 60,
-            'user' => auth('api')->user()->get(['id', 'name', 'email'])
+            'user' => auth('api')->user(),
         ]);
     }
 
